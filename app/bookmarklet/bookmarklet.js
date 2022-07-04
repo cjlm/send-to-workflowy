626a847a3a59d1bc3fe2e1b59d9778bcff0e00b6
cjlm <christian.miles@gmail.com>
2022-07-04 11:33:19 -0700

    Attempt to update the bookmarklet

--- app/bookmarklet/bookmarklet.js
+++ app/bookmarklet/bookmarklet.js
@@ -1,11 +1,16 @@
 post('!URL', {
   sessionId: '!SESSION_ID',
   parentId: '!PARENT_ID',
+  sharedNode: '!SHARED_NODE',
+  mode: '!MODE',
   text: `<a href=%22${document.location.href}%22>${document.title}</a>`,
   note: '',
 });
 
-function post(url, { sessionId, parentId, text, note, priority = 0 }) {
+function post(
+  url,
+  { sessionId, parentId, text, note, priority = 0, sharedNode, mode }
+) {
   fetch(url, {
     method: 'POST',
     mode: 'no-cors',
--- app/components/BookmarkletLink.jsx
+++ app/components/BookmarkletLink.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from '@chakra-ui/react';
 
-import bookmarkletRaw from '/bookmarklet.min.js?raw'
+import bookmarkletRaw from '/bookmarklet.min.js?raw';
 
 export default function BookmarkletLink(props) {
-  const { sessionId, parentId, children } = props;
+  const { sessionId, parentId, sharedNode, mode, children } = props;
   const [link, setLink] = useState('');
 
   useEffect(() => {
@@ -12,9 +12,11 @@ export default function BookmarkletLink(props) {
       `javascript:${bookmarkletRaw
         .replace(`!SESSION_ID`, sessionId)
         .replace(`!PARENT_ID`, parentId)
+        .replace(`!SHARED_NODE`, sharedNode)
+        .replace(`!MODE`, mode)
         .replace(`!URL`, `${document.URL}send`)}`
     );
-  }, [sessionId, parentId]);
+  }, [sessionId, parentId, sharedNode, mode]);
 
   return <Link href={link}>{children}</Link>;
 }
