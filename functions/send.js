2e17a59c23f9212b9da2dd75bde68fff3bbd7372
cjlm <christian@cjlm.ca>
2022-10-18 09:04:49 -0700

    move simple send to new backend

--- functions/send.js
+++ functions/send.js
@@ -21,7 +21,7 @@ async function send(request, env) {
 
     await fetch(url, {
       method: 'POST',
-      headers: isSimple ? {} : { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
     });
 
